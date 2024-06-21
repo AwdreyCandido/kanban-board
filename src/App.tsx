31696d5130abb8fc18acd783d8e438a4adc96198
AwdreyCandido <awdrey.dev@gmail.com>
2024-06-21 19:46:39 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="container">
+      <div className="tasks-container">1</div>
+      <div className="tasks-container">2</div>
+      <div className="tasks-container">3</div>
+    </div>
+  )
+}
+
+export default App
